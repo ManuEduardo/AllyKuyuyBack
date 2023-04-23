77907031b19f1e1d0285da304ead89d0ef83fe24
Rasr1 <nerbat643@gmail.com>
2023-04-23 13:10:56 -0500

    imagenesss

--- src/main/java/com/app/AylluKuyuy/pruebaSubirImagen/FileUploadController.java
+++ src/main/java/com/app/AylluKuyuy/pruebaSubirImagen/FileUploadController.java
@@ -1,10 +1,12 @@
 package com.app.AylluKuyuy.pruebaSubirImagen;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import com.app.AylluKuyuy.modelos.Croquis;
 import com.app.AylluKuyuy.repositories.CroquisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,10 +19,8 @@ public class FileUploadController {
     CroquisRepository croquisRepository;
 
     @PostMapping("/uploadFile")
-    public Croquis uploadFile(
-            @RequestParam("file") MultipartFile multipartFile)
+    public Croquis uploadFile(@RequestParam("file") MultipartFile multipartFile)
             throws IOException {
-
         Croquis croquis = new Croquis();
         croquis.setMapa(convertImageToBytes(multipartFile));
         croquis.setIdfamilia(1);
@@ -29,9 +29,22 @@ public class FileUploadController {
         return croquisRepository.save(croquis);
     }
 
+    /*@GetMapping("/mapa")
+    public ResponseEntity<Croquis> uploadFile(){
+        Croquis croquis = croquisRepository.findById(1).get();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(croquis.getMapa().length);
+        return ResponseEntity.ok().headers(headers).body(croquis);
+    }*/
+
     @GetMapping("/mapa")
-    public Croquis uploadFile(){
-        return croquisRepository.findById(1).get();
+    public ResponseEntity<byte[]> uploadFile(){
+        Croquis croquis = croquisRepository.findById(1).get();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(croquis.getMapa().length);
+        return new ResponseEntity<>(croquis.getMapa(), headers, HttpStatus.OK);
     }
 
     @PostMapping("/pruebita")
